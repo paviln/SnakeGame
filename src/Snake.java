346c2f54b44192da3ebdc1f2daed965d0b1fe458
hedall89 <henrikvadmanddall@gmail.com>
2020-02-29 16:26:49 +0100

    Snake - Wall Collision

--- src/Snake.java
+++ src/Snake.java
@@ -12,6 +12,7 @@ public class Snake
     private ArrayList<Square> squares; //rename to snakeSquares? Otherwise it has the same name as the 2D array in Arena.
     private int xMovement = 0;
     private int yMovement = 0;
+    private boolean gameOver = false;
 
     public Snake(Square square)
     {
@@ -40,23 +41,33 @@ public class Snake
 
     public void move()
     {
+        if (gameOver)
+            return;
         switch (currentDirection)
         {
             case UP:
                 xMovement = 0;
                 yMovement = -25;
+                if (head.getY() == 0 )
+                    gameOver = true;
                 break;
             case DOWN:
                 xMovement = 0;
                 yMovement = 25;
+                if (head.getY() == 350)
+                    gameOver = true;
                 break;
             case LEFT:
                 xMovement = -25;
                 yMovement = 0;
+                if (head.getX() == 0 )
+                    gameOver = true;
                 break;
             case RIGHT:
                 xMovement = 25;
                 yMovement = 0;
+                if (head.getX() == 350)
+                    gameOver = true;
                 break;
             case PAUSE:
                 xMovement = 0;
