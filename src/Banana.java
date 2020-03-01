8fde126a3ca47923439b30628422205290a5909d
linerebo <55693839+linerebo@users.noreply.github.com>
2020-03-01 22:51:59 +0100

    New file Banana

---
+++ src/Banana.java
@@ -0,0 +1,25 @@
+import javafx.scene.image.Image;
+
+
+/**
+ * @author Line
+ */
+
+public class Banana extends Food {
+
+
+    public Banana(Square initPos){
+        super(initPos);
+        foodImage = new Image("banana.PNG");
+    }
+
+    //it is possible to make score depend on type of food
+    public void handlePlayer(Player p){
+        p.score = p.score +1;
+    }
+
+    //to handle velocity or size of snake; depending on type of food
+    public void handleSnake(){
+
+    }
+}
